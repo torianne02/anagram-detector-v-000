3a24b6189bdb89721f2bfaf01130246905d134ec
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-05-14 13:09:44 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(initial_word)
+    @word = initial_word
+  end
+
+  def match(possible_anagrams)
+    matches = []
+    possible_anagrams.each do |anagram_word|
+      if anagram_word.sort == @word.sort
+        matches += anagram_word
+      end
+    end
+    return matches
+  end 
+    # for word in possible_anagrams:
+    #   word.sort()
+    #   if word == initial_word:
+    #     matches += word
+    # return matches
+end
